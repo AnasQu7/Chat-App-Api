ff21b58663ede9fb29e5505525d5dfd5685d8334
Mohd Anas <mohammadanasquraishi78600@gmail.com>
2022-10-05 16:04:23 +0530

    api created

---
+++ server.js
@@ -0,0 +1,22 @@
+import {Server, Socket} from "socket.io"
+
+
+const io = new Server(8000,{
+  cors:{
+    origin: "*"
+  }
+})
+const users = {}
+io.on('connection',(socket)=>{
+  
+    
+     socket.on("new-user-connected" , (name)=>{
+      users[socket.id] = name
+      socket.broadcast.emit('user-joined',name)
+      console.log(name,"joined chat")
+         
+     });
+     socket.on("send" , (message)=>{
+       socket.broadcast.emit('recieve',{message:message,name:users[socket.id]})
+     });
+})
