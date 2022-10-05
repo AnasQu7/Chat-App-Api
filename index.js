004606b5142cdd8e92ba371ce0485d6799b15059
Mohd Anas <mohammadanasquraishi78600@gmail.com>
2022-10-05 20:43:24 +0530

    added disconected

--- index.js
+++ index.js
@@ -19,4 +19,9 @@ io.on('connection',(socket)=>{
      socket.on("send" , (message)=>{
        socket.broadcast.emit('recieve',{message:message,name:users[socket.id]})
      });
-})
+
+     socket.on('disconnect', message=>{
+      socket.broadcast.emit('left',users[socket.id])
+      delete users[socket.id]
+     })
+    })
