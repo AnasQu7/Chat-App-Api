ba8a1fc94bc0cc0d94a368180f3ab03d6fac129e
Mohd Anas <mohammadanasquraishi78600@gmail.com>
2022-10-06 13:37:53 +0530

    bug fixed

--- index.js
+++ index.js
@@ -7,21 +7,36 @@ const io = new Server(8000,{
   }
 })
 const users = {}
+let userdetails=[]
 io.on('connection',(socket)=>{
   
+  
+  socket.on("new-user-connected" , (name)=>{
+    users[socket.id] = name
+    socket.broadcast.emit('user-joined',name)
+    console.log(name,"joined chat")
+    userdetails.push(name)
+    socket.emit('userList',userdetails)
+  });
+  socket.on("send" , (message)=>{
+    socket.broadcast.emit('recieve',{message:message,name:users[socket.id]})
+  });
+  
+  socket.on('disconnect', message=>{
+    socket.broadcast.emit('left',users[socket.id])
+    console.log(users[socket.id])
+    const index = userdetails.indexOf(users[socket.id]);
+    if (index > -1) { 
+      userdetails.splice(index, 1); 
+    }
     
-     socket.on("new-user-connected" , (name)=>{
-      users[socket.id] = name
-      socket.broadcast.emit('user-joined',name)
-      console.log(name,"joined chat")
-         
-     });
-     socket.on("send" , (message)=>{
-       socket.broadcast.emit('recieve',{message:message,name:users[socket.id]})
-     });
-
-     socket.on('disconnect', message=>{
-      socket.broadcast.emit('left',users[socket.id])
-      delete users[socket.id]
+    socket.emit('userList',userdetails)
+    delete users[socket.id]
+   
      })
+
+     socket.on('connection',data=>{
+      console.log(data)
+     })
+
     })
